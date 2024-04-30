fd2dc719a6b7e076b79ba7592f51fdd7cc10271d
keisukethesummer <addict20fx1@gmail.com>
2024-04-30 22:05:40 +0900

    Update App.vue
    フロントの部分を更新しました。

--- shiga-business-matching/src/App.vue
+++ shiga-business-matching/src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <v-select
-  style= "height: 100px; width: 300px; padding:20px; position: relative; top: 20px; left: 40px;"
-  label="業種"
-  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-  ></v-select>
+    <v-select
+    style="height: 100px;
+           width: 380px;
+           padding: 20px;
+           position: relative;
+           top: 20px;
+           left: 5%;"
+    label="業種"
+    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+    ></v-select>
+    <v-btn
+    color="blue"
+    style="height: 50px;
+           width: 340px;
+           border: solid;
+           position: relative;
+           bottom: 50px;
+           left: 70%;
+           "
+    >
+    新規追加
+    </v-btn>
 
-  <v-btn
-  width="300"
-  style="height: 50px; border: solid; position: relative; bottom: 50px; left: 1150px;"
-  >
-  新規追加
-  </v-btn>
-
-  <v-row>
-    <v-col cols="12" md="4">
+  <v-row class="d-flex flex-row justify-space-between box">
+    <v-spacer></v-spacer>
+    <v-col cols="12" md="3">
       <v-card
         subtitle="This is a card subtitle"
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
@@ -21,7 +32,8 @@
       ></v-card>
     </v-col>
 
-    <v-col cols="12" md="4">
+    <v-spacer></v-spacer>
+    <v-col cols="12" md="3">
       <v-card>
         <template v-slot:title>
           This is a title
@@ -37,7 +49,8 @@
       </v-card>
     </v-col>
 
-    <v-col cols="12" md="4">
+    <v-spacer></v-spacer>
+    <v-col cols="12" md="3">
       <v-card>
         <v-card-item>
           <v-card-title>This is a title</v-card-title>
@@ -50,5 +63,6 @@
         </v-card-text>
       </v-card>
     </v-col>
+    <v-spacer></v-spacer>
   </v-row>
 </template>
