f164cdf9318426720a4e5b92d256c0f6823c9372
hakata <hakataon@gmail.com>
2024-05-27 12:33:53 +0900

    コミットします

---
+++ additionhp/src/App.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-app>
+    <v-app-bar class="custom-app-bar" color="orange">
+      <v-toolbar-title>会社概要</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text >TOP</v-btn>
+      <v-btn text >サービス</v-btn>
+      <v-btn text >会社概要</v-btn>
+      <v-btn text >お問い合わせ</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="mt-5">
+        <v-row>
+          <v-col cols="12">
+            <v-card class="mb-5">
+              <v-row>
+                <v-col cols="12" md="8">
+                  <v-card-title>代表挨拶</v-card-title>
+                  <v-card-text>
+                    代表挨拶の例文。<br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。 <br>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。 <br>
+                    <div class="signature">代表者名</div>
+                  </v-card-text>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-img
+                    src="@/assets/logo.png"
+                    aspect-ratio="1"
+                    class="custom-img"
+                  ></v-img>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12">
+            <v-card>
+              <v-card-title>メッセージ</v-card-title>
+              <v-card-text>
+                <div class="mission">
+                  <strong>Mission</strong>
+                  <p>中堅中小企業及び個人事業主のIT活用を推進し、データに基づく企業成長を実現する</p>
+                </div>
+                <div class="vision">
+                  <strong>Vision</strong>
+                  <p>経営に確かな地図とコンパスを</p>
+                </div>
+                <br>
+                <div class="value">
+                  <strong>Value</strong>
+                  <p>
+                    ビジネスを楽しむ<br>
+                    常に新しいことにチャレンジする<br>
+                    顧客の成功にコミットする
+                  </p>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" class="mt-5">
+            <v-card>
+              <v-card-title>会社概要</v-card-title>
+              <v-card-text>
+              <div class="company-info">
+                <p><strong>会社名&nbsp;&nbsp;&nbsp;&nbsp;：</strong>テキストテキストテキストテキストテキストテキスト</p>
+                <p><strong>設立日&nbsp;&nbsp;&nbsp;&nbsp;：</strong>テキストテキストテキストテキストテキストテキスト</p>
+                <p><strong>所在地&nbsp;&nbsp;&nbsp;&nbsp;：</strong>テキストテキストテキストテキストテキストテキスト</p>
+                <p><strong>代表者&nbsp;&nbsp;&nbsp;&nbsp;：</strong>テキストテキストテキストテキストテキストテキスト</p>
+                <p><strong>事業内容：</strong>テキストテキストテキストテキストテキストテキスト</p>
+              </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer color="orange">
+      <v-col class="text-center">
+        <v-spacer></v-spacer>
+        <v-btn text class="custom-btn">TOP</v-btn>
+        <v-btn text class="custom-btn">サービス</v-btn>
+        <v-btn text class="custom-btn">会社概要</v-btn>
+        <v-btn text class="custom-btn" >お問い合わせ</v-btn>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.custom-app-bar {
+  position: relative !important;
+}
+.v-application {
+  min-height: 100vh;
+}
+
+.custom-btn {
+  color: black !important;  /* ボタンのテキストカラーを黒に設定 */
+  background-color: transparent !important;  /* ボタンの背景色を透明に設定 */
+  border: none !important;  /* ボタンの枠線を透明に設定 */
+  box-shadow: none !important; /* ボタンの影を削除*/
+}
+
+.signature {
+text-align: right;
+margin-top: 20px;
+}
+
+.custom-img {
+  height: 400px; /* 画像の高さを指定 */
+}
+
+.v-card-title {
+font-size: 1.5rem;
+font-weight: bold;
+}
+.v-card-text {
+font-size: 1rem;
+}
+
+.mission,vision,value{
+  margin-bottom: 20px;
+}
+
+.company-info p {
+  margin: 0;
+  margin-bottom: 10px;
+}
+
+@media (max-width: 600px) {
+.custom-img {
+height: auto; /* スマホ画面では画像の高さを自動調整 */
+width: 100%; /* 画像を全幅に調整 */
+}
+}
+</style>
--- shiga-business-matching/src/App.vue
+++ shiga-business-matching/src/App.vue
@@ -1,54 +1,312 @@
 <template>
-  <v-select
-  style= "height: 100px; width: 300px; padding:20px; position: relative; top: 20px; left: 40px;"
-  label="業種"
-  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-  ></v-select>
+  <!-- 業種選択・新規登録(画面上部) -->
 
+  <!-- 業種選択プルダウン -->
+  <v-select
+    v-model="selectedBusiness"
+    :items="businessList"
+    label="業種"
+    style="height: 100px;
+          width: 380px;
+          padding: 20px;
+          position: relative;
+          top: 20px;
+          left: 5%;"
+  >
+  </v-select>
+  
+  <!-- 新規登録ボタン -->
   <v-btn
-  width="300"
-  style="height: 50px; border: solid; position: relative; bottom: 50px; left: 1150px;"
+    @click="open"
+    color="blue"
+    style="height: 50px;
+          width: 340px;
+          border: solid;
+          position: relative;
+          bottom: 50px;
+          left: 70%;
+          "
   >
   新規追加
   </v-btn>
 
-  <v-row>
-    <v-col cols="12" md="4">
-      <v-card
-        subtitle="This is a card subtitle"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
-        title="This is a title"
-      ></v-card>
-    </v-col>
+<<<<<<< HEAD
+  <v-dialog v-model="dialog" max-width="400px">
+  <v-card>
+    <v-card-title class="text-h5">情報を入力してください</v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="名前" id="name" outlined dense></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="フリガナ" id="furigana" outlined dense></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-select
+              :items="['男性', '女性']"
+              label="性別"
+              id="gender"
+              outlined
+              dense></v-select>
+          </v-col>
+          <v-col cols="12">
+            <v-date-picker
+              ref="picker"
+              locale="jp-ja"
+              v-model="date"
+              :day-format="date => new Date(date).getDate()"
+              :max="new Date().toISOString().substr(0, 10)"
+              :picker-date="pickerDate"
+              min="1950-01-01"
+              @change="save"
+            ></v-date-picker>
+            </v-col>
+          <v-col cols="12">
+            <v-text-field label="業種" id="industry" outlined dense></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" @click="submitForm">送信</v-btn>
+    </v-card-actions>
+  </v-card>
+</v-dialog>
 
-    <v-col cols="12" md="4">
-      <v-card>
-        <template v-slot:title>
-          This is a title
-        </template>
+=======
+  <v-dialog v-model="dialog" max-width="300">
+    <div 
+      style="background:white;">
+        <h2>情報を入力してください</h2>
+        <div style="margin-bottom: 10px;">
+          <div style="border: 1px solid #ccc; padding: 10px;">
+          <label for="name">名前:</label>
+          <input type="text" id="name">
+        </div>
+      </div>
+      <div>
+        <label for="furigana">フリガナ:</label>
+        <input type="text" id="furigana">
+      </div>
+      <div>
+        <label for="gender">性別:</label>
+        <select id="gender">
+          <option value="male">男性</option>
+          <option value="female">女性</option>
+        </select>
+      </div>
+      <div>
+        <label for="industry">業種:</label>
+        <input type="text" id="industry">
+      </div>
+      <button @click="submitForm">送信</button>
+    </div>
+  </v-dialog>
+>>>>>>> fa3064ebf4b1026c87cdf1c2469f43c2ddb8ddc9
 
-        <template v-slot:subtitle>
-          This is a card subtitle
-        </template>
+  <!-- 登録ユーザ情報をカードで表示(画面下部) -->
+  <v-row class="d-flex flex-row justify-space-between box">
+    <v-spacer></v-spacer>
+    <v-card v-for="user in FilteredUsers"
+            :key="user.id"
+            style="height: 280px;
+                   width:  300px;
+                   margin: 0px 10px;
+                   color:  black;
+                   background: lightgray;
+                  "
+    >
+      <!-- 名前 -->
+      <v-card-title>
+        {{user.name}}
+      </v-card-title>
 
-        <template v-slot:text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
-        </template>
-      </v-card>
-    </v-col>
+      <!-- ふりがな -->
+      <v-card-text>
+      {{user.furigana}}
+      </v-card-text>
 
-    <v-col cols="12" md="4">
-      <v-card>
-        <v-card-item>
-          <v-card-title>This is a title</v-card-title>
+      <!-- 性別 -->
+      <v-card-text>
+      {{user.gender}}
+      </v-card-text>
 
-          <v-card-subtitle>This is a card subtitle</v-card-subtitle>
-        </v-card-item>
+      <!-- 生年月日 -->
+      <v-card-text>
+      {{user.birth}}
+      </v-card-text>
 
-        <v-card-text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
-        </v-card-text>
-      </v-card>
-    </v-col>
+      <!-- 業種 -->
+      <v-card-text>
+      {{user.business}}
+      </v-card-text>
+
+      <!-- 編集ボタン -->
+      <v-row class="justify-end" style="padding-right: 5%">
+        <v-icon @click="openEditModal(user.id)">
+          mdi-pencil
+        </v-icon>
+
+        <!-- モーダル -->
+        <v-dialog v-model="editModal" max-width="400">
+          <v-card
+            style="height: 280px;
+                   width:  300px;
+                   margin: 0px 10px;
+                   color:  black;
+                   background: lightgray;
+                  "
+          >
+          <!-- モーダルの内容(未実装) -->
+          </v-card>
+        </v-dialog>
+
+        <!-- 削除ボタン -->
+        <v-icon @click="openDeleteModal(user.id)">
+          mdi-delete
+        </v-icon>
+
+        <!-- モーダル -->
+        <v-dialog v-model="deleteModal" max-width="400">
+          <v-card>
+            <!-- モーダルの内容 -->
+            <v-text>削除しますか？</v-text>
+            <div>   <!-- ボタンを横並びにするためdivを使用 -->
+              <v-btn @click="deleteUser">削除</v-btn>
+              <v-btn @click="closeDeleteModal">キャンセル</v-btn>
+            </div>
+          </v-card>
+        </v-dialog>
+
+      </v-row>
+    </v-card>
+    <v-spacer></v-spacer>
   </v-row>
+
 </template>
+
+<script>
+export default {
+  /*------*/
+  /* data */
+  /*------*/
+  data() {
+    return {
+      //ここは初期値を定義してあげるだけ（今回は最初は閉じていたいのでfalse)
+      dialog: false,
+
+      editModal: false,
+      deleteModal: false,
+      lookingId: null,
+
+      /* 登録ユーザ情報 */
+      users:
+      [
+        {
+          id: 3,
+          name: "山田太郎",
+          furigana: "ヤマダタロウ",
+          gender: "男性",
+          birth: "1980/3/3",
+          business: "エステサロン経営",
+          isDeleted: false,
+        },
+        {
+          id: 4,
+          name: "田中花子",
+          furigana: "タナカハナコ",
+          gender: "女性",
+          birth: "1970/5/5",
+          business: "生命保険窓口",
+          isDeleted: false,
+        },
+        {
+          id: 5,
+          name: "鈴木史郎",
+          furigana: "スズキシロウ",
+          gender: "男性",
+          birth: "1960/7/7",
+          business: "SNS運用コンサルタント",
+          isDeleted: false,
+        },
+      ],
+      selectedBusiness: "全ての業種",
+    };
+  },
+  methods: {
+    open: function() {
+      // dataのdialogをtrueに書き換えているだけ
+      this.dialog = true;
+      // 逆にfalseにすれば閉じるを実装できる
+    },
+
+    /* ユーザ編集モーダルオープン */
+    openEditModal(id)
+    {
+      this.editModal = true;
+      this.lookingId = id;
+    },
+
+    /* ユーザ削除モーダルオープン */
+    openDeleteModal(id)
+    {
+      this.deleteModal = true;
+      this.lookingId = id;
+    },
+
+    /* ユーザ削除処理(論理削除) */
+    deleteUser()
+    {
+      for(let i = 0; i < this.users.length; i++)
+      {
+        if (this.users[i].id === this.lookingId)
+        {
+          this.users[i].isDeleted = true;
+        }
+      }
+      this.deleteModal = false;
+    },
+
+    /* ユーザ削除モーダルのクローズ */
+    closeDeleteModal()
+    {
+      this.deleteModal = false;
+    },
+  },
+
+  /*----------*/
+  /* computed */
+  /*----------*/
+  computed:
+  {
+    /* 登録されている業種一覧を返す */
+    businessList()
+    {
+      /* Array.from(オブジェクトを配列に変換) */
+      /* new Set(値を重複を許さずコレクションしたオブジェクト) */
+      /* this.users.map(usersの各データを見ていき、業種を取り出す) */
+      return ["全ての業種", ...new Set(this.users.map(user => user.business))];
+    },
+    /* 表示するユーザをフィルタ */
+    FilteredUsers()
+    {
+      if(this.selectedBusiness === "全ての業種")
+      {
+        /* 論理削除されてないユーザをすべて表示 */
+        return this.users.filter(user => !user.isDeleted);
+      }
+      else
+      {
+        return this.users.filter(user => user.business === this.selectedBusiness && !user.isDeleted);
+      }
+    }
+  },
+};
+</script>
+<<<<<<< HEAD
+
+=======
+>>>>>>> fa3064ebf4b1026c87cdf1c2469f43c2ddb8ddc9
