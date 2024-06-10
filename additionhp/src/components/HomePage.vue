eb420b414412291df4f1ccb92b24255498c5b964
hakata <hakataon@gmail.com>
2024-06-11 03:18:38 +0900

    更新します。

--- additionhp/src/components/HomePage.vue
+++ additionhp/src/components/HomePage.vue
@@ -50,7 +50,7 @@
 
     <v-main style="padding: 0%; padding-bottom: 100px;">
       <v-container fluid style="padding: 0; margin: 0;;">
-        <v-row style="padding: 0; margin: 0;">
+        <v-row class="pb-sm-9 pd-0" style="margin: 0;">
                 <v-col cols="12" class="text-center" style="padding: 0; margin: 0;">
                   <v-img style="
                   padding-left: 0%;
@@ -61,32 +61,32 @@
                 </v-col>
         </v-row>
 
-        <v-row class="my-3 justify-center" style="padding-top: 100px;">
-          <v-col cols="12" md="10" style="margin: 0%;">
-            <v-card class="pa-10;">
+        <v-row  class="pt-sm-16 pt-0 justify-center text-sm-left text-center" style="margin-top: 0;">
+          <v-col cols="12" style="margin: 0; padding: 0;" >
+            <v-card class="pa-10;" style="box-shadow: none;">
               <v-card-title
               class="text-center"
               >会社概要
               </v-card-title>
               <v-card-text>
                 <v-row>
-                  <v-col cols="12" sm="4" md="4" lg="4" xl="4" xs="12">
+                  <v-col cols="12" sm="5" md="5" lg="5" xl="5">
                     <v-img 
                     src="@/assets/logo.png"
                     style="width: 100%; height: auto;"
                     ></v-img>
                   </v-col>
-                  <v-col cols="12" sm="8" md="8" lg="8" xl="8">
+                  <v-col cols="12" sm="7" md="7" lg="7" xl="7">
                     <v-card style="box-shadow: none;" >
-                      <v-card-text style="font-size: breaksize1;">
-                        &nbsp;&nbsp;&nbsp;ITをもっと身近に、<br>
-                        &nbsp;&nbsp;&nbsp;人々の生活にもっとITを。<br>
+                      <v-card-text style="font-size: 1.5rem; font-weight: bold;" class="text-sm-h3">
+                        ITをもっと身近に、<br>
+                        人々の生活にもっとITを。<br>
                       </v-card-text>
                       <v-card-text>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
                       </v-card-text>
                     </v-card>
                   </v-col>
@@ -102,68 +102,68 @@
           </v-col>
         </v-row>
 
-        <v-row style="margin-right: 0; padding-left: 100px; padding-right: 100px; padding-top: 100px;" >
-              <v-col cols="12" style="margin: 0% auto;">
-                <v-card class="pa-10;" style="margin-top: 0; box-shadow: none;">
-                  <v-card-title class="text-center">サービス</v-card-title>
+        <v-row class="padding-setting" style="margin-right: 0; padding-left: 100px; padding-right: 100px; padding-top: 100px;" >
+            <v-col cols="12" style="margin: 0% auto;">
+              <v-card class="pa-10;" style="margin-top: 0; box-shadow: none;">
+                <v-card-title class="text-center">サービス</v-card-title>
                   <v-card-text>
                     <v-row>
-                      <v-col cols="12" md="6" style="padding-bottom: 40px; padding-top: 40px;">
-                        <v-img 
-                        src="@/assets/IT_m.jpg"
-                        style="width: 100%; height: 100%; "
-                        ></v-img>
+                        <v-col class="position-change1" cols="12" md="6" style="padding-bottom: 40px; padding-top: 40px;">
+                          <v-img 
+                          src="@/assets/IT_m.jpg"
+                          style="width: 100%; height: 100%; "
+                          ></v-img>
+                        </v-col>
+                      <v-col class="position-change2" cols="12" md="6" style="display: flex; flex-direction: column; justify-content: flex-end; margin-bottom: 35px;">
+                          <v-card style="box-shadow: none;">
+                            <v-card-text class="text-sm-left text-center" style="font-size: 2rem; font-weight: bold; text-align: left; margin-top: 40px; text-decoration: underline;">
+                              IT Planners<br>
+                            </v-card-text>
+                            <v-card-text style="text-align: left;">
+                              DX推進や業務効率化などビジネスにおける課題解決に向けた 技術的知見、<br>
+                              開発リソースを月3万円〜で提供します。
+                            </v-card-text>
+                          </v-card>
+                          <div style="margin-top: auto; text-align: right; width: 100%;">
+                            <v-btn to="/pr" style="background-color: #F1684D!important; font-size: 1rem; font-weight: bold; color: black; padding: 0%;">詳しくはこちらから＞＞</v-btn>
+                          </div>
                       </v-col>
-                      <v-col cols="12" md="6" style="display: flex; flex-direction: column; justify-content: flex-end; margin-bottom: 35px;">
-                        <v-card style="box-shadow: none;">
-                          <v-card-text style="font-size: 2rem; font-weight: bold; text-align: left; margin-top: 40px; text-decoration: underline;">
-                            IT Planners<br>
-                          </v-card-text>
-                          <v-card-text style="text-align: left;">
-                        DX推進や業務効率化などビジネスにおける課題解決に向けた 技術的知見、<br>
-                        開発リソースを月3万円〜で提供します。
-                      </v-card-text>
-                    </v-card>
-                    <div style="margin-top: auto; text-align: right; width: 100%;">
-                      <v-btn to="/pr" style="background-color: #F1684D!important; font-size: 1rem; font-weight: bold; color: black; padding: 0%;">詳しくはこちらから＞＞</v-btn>
-                    </div>
-                  </v-col>
-                  
-                  <v-col cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px;">
-                    <v-card style="box-shadow: none; text-align: left;">
-                      <v-card-text style="font-size: 2rem; font-weight: bold; margin-top: 40px">
-                        プログラミング教育<br>
-                      </v-card-text>
-                      <v-card-text style="text-align: left;">
-                        一定のレベルまでプログラムレベルを引き上げながら、報酬を受け取れる<br>
-                        開発の仕事に参加し、実践的に業務がこなしながら学べるスクールの運営
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px; padding-left: 30px;">
-                    <v-img 
-                    src="@/assets/programing_m.jpg"
-                    style="width: 100%; height: 100%;"
-                    ></v-img>
-                  </v-col>
+                    
+                    <v-col class="position-change4" cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px;">
+                      <v-card style="box-shadow: none; text-align: left;">
+                        <v-card-text class="text-sm-left text-center" style="font-size: 2rem; font-weight: bold; margin-top: 40px">
+                          プログラミング教育<br>
+                        </v-card-text>
+                        <v-card-text style="text-align: left;">
+                          一定のレベルまでプログラムレベルを引き上げながら、報酬を受け取れる<br>
+                          開発の仕事に参加し、実践的に業務がこなしながら学べるスクールの運営
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
+                    <v-col class="position-change3" cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px; padding-left: 30px;">
+                      <v-img 
+                      src="@/assets/programing_m.jpg"
+                      style="width: 100%; height: 100%;"
+                      ></v-img>
+                    </v-col>
 
-                  <v-col cols="12" md="6" style="padding-top: 40px; padding-bottom: 40px;">
-                    <v-img 
-                    src="@/assets/system_m.jpg"
-                    style="width: 100%; height: 100%; "
-                    ></v-img>
-                  </v-col>
-                  <v-col cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px;">
-                    <v-card style="box-shadow: none;">
-                      <v-card-text style="font-size: 2rem; font-weight: bold; text-align: left; margin-top: 40px">
-                        システム構築<br>
-                      </v-card-text>
-                      <v-card-text style="text-align: left;">
-                        サービス開発、アプリ開発、LINEbot開発、HP/LP制作などリクエストに応じた<br>
-                        開発を行います。
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
+                    <v-col class="position-change5" cols="12" md="6" style="padding-top: 40px; padding-bottom: 40px;">
+                      <v-img 
+                      src="@/assets/system_m.jpg"
+                      style="width: 100%; height: 100%; "
+                      ></v-img>
+                    </v-col>
+                    <v-col class="position-change6" cols="12" md="6" style=" padding-top: 40px; padding-bottom: 40px;">
+                      <v-card style="box-shadow: none;">
+                        <v-card-text class="text-sm-left text-center" style="font-size: 2rem; font-weight: bold; text-align: left; margin-top: 40px">
+                          システム構築<br>
+                        </v-card-text>
+                        <v-card-text style="text-align: left;">
+                          サービス開発、アプリ開発、LINEbot開発、HP/LP制作などリクエストに応じた<br>
+                          開発を行います。
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
                 </v-row>
               </v-card-text>
             </v-card>
@@ -199,18 +199,6 @@
 
 <script>
 export default {
-  computed:{
-    breaksize1(){
-      switch(this.$vuetify.breakpoint.name){
-        case 'xs': return 3
-        case 'sm': return 32
-        case 'md': return 48
-        case 'lg': return 56
-        case 'xl': return 80
-      }
-    },
-  },
-
 data(){
   return{
   drawer: false,
@@ -264,13 +252,37 @@ methods:{
   background-color: orange;
   font-weight: bold !important;
   z-index: 1000;	
-align-items: center;	
-font-size: 1.25rem;
-border-radius: 10px;
+  align-items: center;	
+  font-size: 1.25rem;
+  border-radius: 10px;
 }	
+
 .floating-btn v-icon {	
 margin-right: 10px;
 /*font-size: 1.5rem;  アイコンのサイズを大きくする */
 }
 
+@media(max-width: 600px) {
+  .padding-setting {
+    padding: 0 !important;
+  }
+  .position-change1{
+    order: 1 !important;
+  }
+  .position-change2{
+    order: 2 !important;
+  }
+  .position-change3{
+    order: 3 !important;
+  }
+  .position-change4{
+    order: 4 !important;
+  }
+  .position-change5{
+    order: 5 !important;
+  }
+  .position-change6{
+    order: 6 !important;
+  }
+}
 </style>
